39aa6d832b6a539dabd550f2c1cc63888dd9aa35
IslamDevFull <it.front.back@gmail.com>
2024-08-19 12:29:41 +0500

    0.1.1v

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // /////////////////////////////////////////////////// animation scroll //////////////////////////////////////////////////
 // document.addEventListener('scroll',()=>{
-//     if(document.documentElement.scrollTop > 50){
-//         document.getElementById('nav').style.padding = '0px 5px'
-//     }
-//     else{
-//         document.getElementById('nav').style.padding = '30px 5px'
-//     }
+//   if(document.documentElement.scrollTop > 50){
+//       document.getElementById('nav').style.backgroundColor = 'rgb(255, 255, 255,1)'
+//   }
+//   else{
+//       document.getElementById('nav').style.backgroundColor = 'rgb(255, 255, 255,0.5)'
+//   }
 // })
 // /////////////////////////////////////////////////// Menyu left drop //////////////////////////////////////////////////
 function openNav() {
@@ -14,61 +14,5 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-}
-// /////////////////////////////////////////////////// Carousel //////////////////////////////////////////////////
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  
-}
-// /////////////////////////////////////////////////// Carousel2 //////////////////////////////////////////////////
-let slideIndex1 = 1;
-showSlides1(slideIndex1);
-
-function plusSlides1(n) {
-  showSlides1(slideIndex1 += n);
-}
-
-function currentSlide1(n) {
-  showSlides1(slideIndex1 = n);
-}
-
-function showSlides1(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides1");
-  let dots = document.getElementsByClassName("dot1");
-  if (n > slides.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active1", "");
-  }
-  slides[slideIndex1-1].style.display = "block";
-  dots[slideIndex1-1].className += " active1";
-  
 }
 
