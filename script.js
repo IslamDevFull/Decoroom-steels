251ac6dbd47a042863ea335f97c3415af535b444
IslamDevFull <it.front.back@gmail.com>
2024-08-19 18:13:37 +0500

    0.1.3v

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // /////////////////////////////////////////////////// animation scroll //////////////////////////////////////////////////
-// document.addEventListener('scroll',()=>{
-//   if(document.documentElement.scrollTop > 50){
-//       document.getElementById('nav').style.backgroundColor = 'rgb(255, 255, 255,1)'
-//   }
-//   else{
-//       document.getElementById('nav').style.backgroundColor = 'rgb(255, 255, 255,0.5)'
-//   }
-// })
+document.addEventListener('scroll',()=>{
+  if(document.documentElement.scrollTop > 50){
+      document.getElementById('nav').style.backgroundColor = 'rgb(3,15,28,1)'
+  }
+  else{
+      document.getElementById('nav').style.backgroundColor = 'rgb(3,15,28,0.5)'
+  }
+})
 // /////////////////////////////////////////////////// Menyu left drop //////////////////////////////////////////////////
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
